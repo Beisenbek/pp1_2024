bf5f6263532243a81946470c4606194ffe7e0119
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-09-03 20:11:14 +0500

    first commit

---
+++ week1/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    cout << a + b << " " <<  "ok";
+
+    
+    return 0;
+}
---
+++ week1/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    cout << a + b <<  "\nok test tesst";
+
+    
+    return 0;
+}
