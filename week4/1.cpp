f421482e12f3dae0de7f605c63eb72e4ba42b926
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-09-24 18:53:58 +0500

    upd

---
+++ week4/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a[10];
+    cin >> a[9];
+    cin >> a[0];
+    cin >> a[1];
+    cin >> a[2];
+    cin >> a[4];
+    cin >> a[5];
+    cin >> a[6];
+    cin >> a[7];
+    cin >> a[8];
+    
+
+    cout << a[0] << " " <<  a[1] << " " << a[2] << " " << a[3] << " " << a[4] << " " << a[5] << " " << a[6] << " " << a[7] << " " << a[8] << " " << a[9] << endl;
+
+ 
+    return 0;
+}
