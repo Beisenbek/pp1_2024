053207772478817721f7b834384e0752d43bc0a9
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2024-11-26 20:01:10 +0500

    'upd'

--- week13/circle.h
+++ week13/circle.h
@@ -10,7 +10,6 @@ struct circle{
         return my_pi * r * r;
     }
     circle operator +(const circle & other){
-        other.r = 12;
         circle result(this->r + other.r);
         return result;
     }
